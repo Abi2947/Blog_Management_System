621fa3159ce74f8b58d4b9cfbeab6b29eb7e3c35
Abinash gautam <94129535+Abi2947@users.noreply.github.com>
2025-09-23 15:39:39 +0545

    Contains Schema of user i.e. username, email, password.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, trim: true },
+  username: { type: String, required: true, trim: true },
   email: {
     type: String,
     required: true,
